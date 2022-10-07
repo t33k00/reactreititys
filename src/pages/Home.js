631a1e7752946af191ed7178f815be531b85e5b1
t33k00 <tmkyllonen@gmail.com>
2022-10-07 12:45:06 +0300

    -

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+    <h3>Home</h3>
+    <Link to="/about">About</Link>
+    
+    </div>
+  )
+}
